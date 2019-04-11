7c2f62228f5a3d0522de1f7d509bff71872603d7
Francis Mendez <fmendez884@gmail.com>
2019-04-11 14:32:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def line(katz_deli)
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
-      puts "The line is currently: #{newline.join(', ')}"
+      puts "The line is currently: #{newline.join('')}"
     end
 end 
